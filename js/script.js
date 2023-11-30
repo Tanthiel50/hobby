a2288d09cf018982f9b792d283561c350f73eebc
Tanthiel50 <66637911+Tanthiel50@users.noreply.github.com>
2023-11-30 22:49:32 +0100

    winner

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let competitors = [
+const competitors = [
     ['End Game', '../images/competitor/competitor1.jpg' , 'est un robot de combat de la série télévisée BattleBots. Il est construit par Jack Barker et son fils Jack Barker Jr. Il est connu pour avoir remporté la saison 5.0 de BattleBots, et pour avoir été le premier robot à avoir remporté deux fois le titre de champion de BattleBots.'],
     ['Huge', '../images/competitor/competitor2.jpg', 'est un robot de combat de la série télévisée BattleBots. Il est construit par Jack Barker et son fils Jack Barker Jr. Il est connu pour avoir remporté la saison 5.0 de BattleBots, et pour avoir été le premier robot à avoir remporté deux fois le titre de champion de BattleBots.' ],
     ['Warhead', '../images/competitor/competitor3.jpg', 'Lorem Warhead' ],
@@ -10,81 +10,36 @@ var playerSelection = null;
 
 
 // Création de la liste des compétiteurs full
-function competitorsListe(competitors){
-    let currentCompetitors = [];
-    for (let i = 0; i < competitors.length; i++) {
-        currentCompetitors.push(competitors[i][currentRound]);
+function competitorsListe(listarevoir){
+    let actuelcomp = [];
+    for (let i = 0; i < listarevoir.length; i++) {
+        actuelcomp.push(listarevoir[i][currentRound]);
     }
-    return currentCompetitors;
-}
-console.log(competitorsListe(competitors));
-
-
-// Selection d'un compétiteur1 aléatoire
-// function competitor1Random(){
-//     var currentCompetitors = competitorsListe(competitors);
-//     const competitor1 = Math.floor(Math.random() * currentCompetitors.length);
-//     console.log(competitor1, currentCompetitors[competitor1]);
-// }
-// competitor1Random();
-
+    return actuelcomp;
+};
 // Selection des opposants aléatoire
-function opponent(){
-    var currentCompetitors = competitorsListe(competitors);
-    const competitor1 = Math.floor(Math.random() * currentCompetitors.length);
-    const competitor2 = Math.floor(Math.random() * currentCompetitors.length);
+function opponent(listeComp){
+    const concurrent =[];
+    const competitor1 = Math.floor(Math.random() * listeComp.length);
+    let competitor2 = Math.floor(Math.random() * listeComp.length);
     while (competitor1 === competitor2) {
-        competitor2 = Math.floor(Math.random() * currentCompetitors.length);
+        competitor2 = Math.floor(Math.random() * listeComp.length);
         }
-        const element1 = dictionnaire[cles[index1]];
-        const element2 = dictionnaire[cles[index2]];
-    
-        return { element1, element2 };
-    }
-    
-    // if (competitor1 != competitor2){
-    //     console.log(currentCompetitors[competitor1]);
-    //     console.log(currentCompetitors[competitor2]);
-    // } else{
-    //     do {
-    //         console.log(currentCompetitors[competitor1]);
-    //         console.log(currentCompetitors[competitor2]);
-    //     }while (competitor1 == competitor2){
-    //         var competitor2 = Math.floor(Math.random() * currentCompetitors.length);
-    //     }
-    // }
+        concurrent.push(listeComp[competitor1],listeComp[competitor2]);
+
+
+        console.log(concurrent);
+        return concurrent;
+    };
+
+function Battle(machintrucraslecul){
+    const gagnant = Math.floor(Math.random() * machintrucraslecul.length)
+    console.log(machintrucraslecul[gagnant]);
+
 }
-opponent();
 
-
-
-
-
-
-
-    // var machin= [{
-    //     'name': 'End Game',
-    //     'numero': 5,
-    // }];
-    // var truc= [{
-    //     'name': 'Witch Doctor',
-    //     'mabite': 'oui',
-    //     'numero': 3,
-    // }];
-
-//     function resultatMatch(competitor1, competitor2){
-        
-//         let resultat= '';
-//         if (competitor1['numero'] > competitor2['numero']){
-//             resultat = "le 1 gagne";
-//         // }else(competitor1['numero'] < competitor2['numero']){
-//         //     resultat = "le 2 gagne";
-//         // }
-//         }else{
-//             resultat = "le 2 gagne";
-//         };
-//         return resultat;
-//     }
-
-
-// console.log(resultatMatch(machin,truc));
+console.log(competitorsListe(competitors));
+let liste2 = competitorsListe(competitors);
+opponent(liste2);
+let liste3 = opponent(liste2);
+Battle(liste3);
